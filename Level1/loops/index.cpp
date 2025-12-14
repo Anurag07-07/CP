70c569532e26dd5baf5056525a5be54dde1d6be9
Anurag07-07 <adityanv8@gmail.com>
2025-12-15 02:04:02 +0530

    cp lec 2

---
+++ Level1/loops/index.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void greet(string name){
+  cout<<"Hello "<<name<<endl;
+}
+
+int main(int argc, char const *argv[])
+{
+  // int a[5] = {1,2,3,4,5};
+  // cout<<a[0]<<" ";
+  // cout<<a[1]<<" ";
+  // cout<<a[2]<<" ";
+  // cout<<a[3]<<" ";
+  // cout<<a[4]<<" ";
+
+  // int a[5];
+  // for (int i = 0; i < 5; i++) cin>>a[i];
+
+  // for(auto &i:a) cin>>i;
+  // for(auto i :a) cout<<i<<" ";
+ 
+  // string name; getline(cin,name);
+  // greet(name);
+
+  // cout<<INT_MAX<<" ";
+  // cout<<INT_MIN<<" ";
+  
+  double a;cin>>a;
+  cout<<fixed<<setprecision(5)<<a;
+
+  return 0;
+}
